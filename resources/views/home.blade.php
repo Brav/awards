bd67391d669556b0177e18cc3232e9c6f9192b04
DESKTOP-OVCDGTL\dushan <dushan887@gmail.com>
2021-06-28 11:29:19 +0200

    css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -117,7 +117,7 @@
                 </div>
 
                 <div class="row">
-                    
+
                     @foreach ($awards as $award)
                         <div class="col-lg-4 mb-4 {{ $award['options']['office_type'] }} award">
 
@@ -125,13 +125,13 @@
                                 <div class="d-block bg-primary-sky w-100 pb-150 ds-image-item"></div>
                                 <div class="ds-image-overlay ">
                                     <div class="ds-image-overlay-content align-items-end text-center px-3">
-                                        <img src="{{ asset('media/images/VP_Awards_Icon_GEM.png')}}" alt="" class="d-block m-auto" style="max-width: 80px;">
-                                        <h3 class="h3 text-white my-3">{{ $award->name }} <span class="font-w400 d-block pt-2">{{ $award->period }}</span></h3>
+                                        <img src="{{ asset('media/images/VP_Awards_Icon_GEM.png')}}" alt="" class="d-block m-auto" style="max-width: 100px;">
+                                        <h3 class="h3 text-primary my-3">{{ $award->name }} <span class="font-w400 d-block pt-2">{{ $award->period }}</span></h3>
                                     </div>
                                 </div>
                                 <div class="ds-image-overlay2 ">
                                     <div class="ds-image-overlay-content text-center px-5">
-                                        <p class="text-white mb-0">
+                                        <p class="text-primary mb-0">
                                             {{ $award->description }}
                                         </p>
                                     </div>
