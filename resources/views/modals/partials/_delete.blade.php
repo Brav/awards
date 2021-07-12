6201811307ec73942f5263352317b17039206cad
Brav <imalimalomrk@gmail.com>
2021-07-12 17:00:42 +0200

    delete dialog added

--- resources/views/modals/partials/_delete.blade.php
+++ resources/views/modals/partials/_delete.blade.php
@@ -10,8 +10,15 @@
 
         <h5 class="text-center">Are you sure you want to delete {{ $itemName }} ?</h5>
     </div>
-    <div class="modal-footer">
+
+    <div class="modal-footer modal-action">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-        <button type="submit" class="btn btn-danger">Delete</button>
+        <button type="button" class="btn btn-danger modal-action-delete">Delete</button>
+    </div>
+
+    <div class="modal-footer dialog d-none">
+        <span class="text-center text-danger">Be careful, you will delete this item</span>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+        <button type="submit" class="btn btn-danger dialog-yes">Yes</button>
     </div>
 </form>
