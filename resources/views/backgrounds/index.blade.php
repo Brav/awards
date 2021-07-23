7a08bb35daf028fae37d4527630ddb25f1a1bd68
Brav <imalimalomrk@gmail.com>
2021-07-23 11:16:21 +0200

    additional checks and warnings added to the background upload

--- resources/views/backgrounds/index.blade.php
+++ resources/views/backgrounds/index.blade.php
@@ -4,6 +4,7 @@
 
     <div class="container mt-5">
         <h2>Upload Images</h2>
+        <p class="text-danger">You can upload only <strong>jpg, jpeg and png</strong> images up to 2mb in size</p>
         <div>
             <form id="multi-file-upload-ajax" method="POST"  action="javascript:void(0)" accept-charset="utf-8" enctype="multipart/form-data">
             @csrf
@@ -12,6 +13,9 @@
                         <div class="form-group">
                             <input type="file" name="files[]" id="files" placeholder="Choose files" multiple >
                         </div>
+                    </div>
+                    <div class="col-md-12">
+                        <i class="fas fa-sync fa-spin d-none" aria-hidden="true"></i>
                     </div>
                     <div class="col-md-12">
                         <button type="submit" class="btn btn-primary" id="submit">Submit</button>
@@ -24,13 +28,13 @@
     <div class="container mt-2">
         <div class="row">
         @if ($images)
-            <div class="col-md-12" id="images-container">                
-                <div class="row">
+            <div class="col-md-12">
+                <div class="row" id="images-container">
                 @foreach ($images as $image)
                     @php
                         $data = pathinfo($image);
                     @endphp
-                    <div class="col-lg-4 col-xl-2" id="{{ $data['filename'] }}">
+                    <div class="col-lg-4 col-xl-2 mb-2 border-bottom border-danger" id="{{ $data['filename'] }}">
 
                         <a class="btn btn-danger btn-sm background-delete"
                         data-file="{{ $image }}"
@@ -43,7 +47,7 @@
                             data-file="{{ $image }}"
                             data-url="{{ route('backgrounds.update') }}">Set as Default</button>
 
-                        <div class="d-block bg-image w-100 pb-100 ds-image-item mt-2" style="background-image: url({{ Storage::url($image) }})"></div>
+                        <div class="d-block bg-image w-100 pb-100 ds-image-item my-2" style="background-image: url({{ Storage::url($image) }})"></div>
                     </div>
                 @endforeach
                 </div>
@@ -65,16 +69,30 @@
         $('#multi-file-upload-ajax').submit(function(e) {
 
             e.preventDefault();
-            let formData = new FormData(this);
+
+            const validImageTypes = ['image/jpeg', 'image/png'];
+
+            let formData   = new FormData(this);
             let totalFiles = $('#files')[0].files.length;
+
+            let $this = $(this)
 
             let files = $('#files')[0];
 
-            for (let i = 0; i < totalFiles; i++) {
-                formData.append('files' + i, files.files[i]);
+            for (let i = 0; i < totalFiles; i++)
+            {
+                let filesize = Math.round((files.files[i].size / 1024));
+
+                if (validImageTypes.includes(files.files[i].type) && filesize < 2049)
+                {
+                    formData.append('files' + i, files.files[i]);
+                }
             }
 
             formData.append('totalFiles', totalFiles);
+
+            $('.fa-spin').removeClass('d-none')
+            $('body').addClass('avoid-clicks')
 
             $.ajax({
                 type:'POST',
@@ -94,7 +112,7 @@
                             let image = `/storage/backgrounds/${element}.png`
                             let html  =
                             `
-                            <div class="col-md-4" id="${element}">
+                            <div class="col-lg-4 col-xl-2 mb-2 border-bottom border-danger" id="${element}">
 
                                 <a class="btn btn-danger btn-sm background-delete"
                                 data-file="public/backgrounds/${element}.png"
@@ -104,8 +122,7 @@
                                     data-file="${element}.png"
                                     data-url="{{ route('backgrounds.update') }}">Set as Default</button>
 
-                                <img class="img-thumbnail"
-                                src="${image}">
+                                <div class="d-block bg-image w-100 pb-100 ds-image-item my-2" style="background-image: url(${image})"></div>
                              </div>
                             `
                             $('#images-container').prepend(html)
@@ -113,10 +130,15 @@
                     }
 
                 },
-                error: function(data){
-                    alert(data.responseJSON.errors.files[0]);
-                    console.log(data.responseJSON.errors);
+                error: function(data)
+                {
+                    alert('Something went wrong. Please try again or try another image')
                 }
+            }).always(function()
+            {
+                $('.fa-spin').addClass('d-none')
+                $('body').removeClass('avoid-clicks')
+                $this.trigger('reset');
             });
         });
     });
