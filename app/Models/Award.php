3f03913e5cdfc95ce20ffd2ddbb0cdecc24c1c43
Brav <imalimalomrk@gmail.com>
2022-04-14 11:55:45 +0200

    Issues fixed

--- app/Models/Award.php
+++ app/Models/Award.php
@@ -343,7 +343,7 @@ class Award extends Model
 
         if(!isset($this->background['logo']))
         {
-            return Storage::url('public/logos/' . $defaultLogo->name);
+            return Storage::url('public/logos/' . $defaultLogo->name ?? '');
         }
 
         return $logo;
