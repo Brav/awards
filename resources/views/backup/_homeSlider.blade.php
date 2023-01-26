3048c53f2ae1d968f18a00bebe4cfa01ceee230e
Brav <imalimalomrk@gmail.com>
2023-01-26 09:27:56 +0100

    WIP

---
+++ resources/views/backup/_homeSlider.blade.php
@@ -0,0 +1,92 @@
+<div class="position-relative overflow-hidden">
+        <div class="js-slider slick-nav-black slick-dotted-inner slick-dotted-white" data-dots="true" data-arrows="true">
+            <!-- slide -->
+            <div>
+                <div class="bg-image d-flex w-100" style="background-image: url( {{ asset('media/images/Clayfield5.jpg')}} ); height: 90vh; min-height: 500px; background-position: top center !important;">
+                    <div class="d-flex align-items-center justify-content-center w-100 h-100" style="background: rgba(0, 0, 0, .4);">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12 pt-5 mt-5">
+
+                                    <h2 class="text-white display-4 pt-5 mt-5"><strong>VetPartners Excellence Awards</strong></h2>
+                                    <p class="text-white h3" style="opacity: .85;">Submit a nomination below by clicking on the award you wish to nominate for</p>
+                                    <a href="#main-container" class="btn btn-hero btn-hero-primary btn-hero-lg m-1 d-scroll">Find out more</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end slide -->
+
+            <!-- slide -->
+            <div>
+                <div class="d-flex w-100" style="height: 90vh; min-height: 500px; background-position: top center !important;">
+     <div class="d-flex align-items-center justify-content-center w-100 h-100" style="background: #258f90;">
+         <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-sm-6 my-1">
+                            <div class="d-block" style="padding-bottom: 95%; background-image: url({{ asset('media/backgrounds/elisa.JPG')}} ); background-size: cover; background-position: center;"></div>
+                        </div>
+                        <div class="col-sm-6 my-1">
+                            <div class="d-block" style="padding-bottom: 95%; background-image: url({{ asset('media/backgrounds/barbara_and_tash_from_shirley_vets.jpg')}} ); background-size: cover; background-position: center;"></div>
+                        </div>
+                        <div class="col-sm-6 my-1">
+                            <div class="d-block" style="padding-bottom: 95%; background-image: url({{ asset('media/backgrounds/marsden_ceremony_photo.jpg')}} ); background-size: cover; background-position: center;"></div>
+                        </div>
+                        <div class="col-sm-6 my-1">
+                            <div class="d-block" style="padding-bottom: 95%; background-image: url({{ asset('media/backgrounds/bvsc.jpg')}} ); background-size: cover; background-position: center;"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="d-block" style="padding-bottom: 82%; background-image: url({{ asset('media/backgrounds/right.png')}} ); background-size: cover; background-position: center;"></div>
+                    <p class="text-white text-center h3" style="opacity: .85;">Submit a nomination below by clicking on the award you wish to nominate for</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+            </div>
+            <!-- end slide -->
+
+            <!-- slide -->
+            <div>
+                <div class="bg-image d-flex w-100" style="background-image: url({{ asset('media/images/QuakersHill.jpg')}}); height: 90vh; min-height: 500px; background-position: top center !important;">
+                    <div class="d-flex align-items-center justify-content-center w-100 h-100" style="background: rgba(0, 0, 0, .4);">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-md-12 pt-5 mt-5">
+
+                                    <h2 class="text-white display-4 pt-5 mt-5"><strong>VetPartners Excellence Awards</strong></h2>
+                                    <p class="text-white h3" style="opacity: .85;">Submit a nomination below by clicking on the award you wish to nominate for</p>
+                                    <a href="#main-container" class="btn btn-hero btn-hero-primary btn-hero-lg m-1 d-scroll">Find out more</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end slide -->
+        </div>
+
+        <div class="ds-seperator">
+
+            <a class="page-scroll fusion-one-page-text-link" href="#">
+                <div class="lineholderfirst"></div>
+            </a>
+
+            <div class="lineholder">
+                <svg version="1.1" class="firstdivider" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0.5px" y="0.5px" width="2560px" height="100px" viewBox="0 0 2560 100" style="enable-background:new 0 0 2560 100;" xml:space="preserve">
+                    <path class="st0" id="linec1c" d="M1300.8,55.1 M1259.2,48 M1269.8,47.6l10,11.9l10.3-11.9C1290.2,47.7,1269.4,47.6,1269.8,47.6z M1280,34.2c-9.7,0-17.6,7.9-17.6,17.6s7.9,17.6,17.6,17.6s17.6-7.9,17.6-17.6S1289.7,34.2,1280,34.2z" shape-rendering="auto" transform="translate(0.5,0.5)"></path>
+                    <path class="st0" id="linec1r" d="M2560,36c0,0-108.5-19-225-19c-282.8,0-413,74-666.5,74c-138.6,0-256.9-17.1-367.7-35.9" shape-rendering="auto" transform="translate(0.5,0.5)"></path>
+                    <path class="st0" id="linec1l" d="M1259.2,48C1141.6,27.7,1030.9,7.8,911,7.8C706.2,7.8,484,96,229.5,96C107,96,0,80.5,0,80.5" shape-rendering="auto" transform="translate(0.5,0.5)"></path>
+                </svg>
+
+            </div>
+
+        </div>
+
+    </div>
