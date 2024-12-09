7784ca808204fa7fee35441acea8dcaab46aa410
Brav <imalimalomrk@gmail.com>
2024-12-09 21:15:53 +0100

    WIP - new UI look

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,27 +1,90 @@
 <!-- Footer -->
-<section class="d-none d-sm-none d-md-block">
-	<img  title="line1g" src="{{ asset('media/svg/line1g.svg')}}" class="img-fluid" style="margin-bottom: -2px;">
-</section>
-<footer id="page-footer" class="bg-primary-dark pt-5">
-    <div class="content py-5">
-        <div class="row">
-        	<div class="col-lg-4">
-        		<h4 class="text-white mb-0">Join Us. Grow Together.</h4>
-        	</div>
+{{--<section class="d-none d-sm-none d-md-block">--}}
+{{--	<img  title="line1g" src="{{ asset('media/svg/line1g.svg')}}" class="img-fluid" style="margin-bottom: -2px;">--}}
+{{--</section>--}}
+{{--<footer id="page-footer" class="bg-primary-dark pt-5">--}}
+{{--    <div class="content py-5">--}}
+{{--        <div class="row">--}}
+{{--        	<div class="col-lg-4">--}}
+{{--        		<h4 class="text-white mb-0">Join Us. Grow Together.</h4>--}}
+{{--        	</div>--}}
 
-        	<div class="col-lg-4">
-        		<p class="text-white"><strong>Phone:</strong>&nbsp;<a href="tel:+610280260116">02 8026 0116</a><br />
-				<strong>Email:</strong>&nbsp;<a href="mailto:hr@vetpartners.com.au">hr@vetpartners.com.au</a><br /></p>
-        	</div>
+{{--        	<div class="col-lg-4">--}}
+{{--        		<p class="text-white"><strong>Phone:</strong>&nbsp;<a href="tel:+610280260116">02 8026 0116</a><br />--}}
+{{--				<strong>Email:</strong>&nbsp;<a href="mailto:hr@vetpartners.com.au">hr@vetpartners.com.au</a><br /></p>--}}
+{{--        	</div>--}}
 
-            <div class="col-lg-4">
-                <p class="text-white">
-                <a href="javascript:void(0)">Unit 1, 277 Lane Cove Road,<br />Macquarie Park, NSW 2113<br /> ACN 114 962 453</a></p>
+{{--            <div class="col-lg-4">--}}
+{{--                <p class="text-white">--}}
+{{--                <a href="javascript:void(0)">Unit 1, 277 Lane Cove Road,<br />Macquarie Park, NSW 2113<br /> ACN 114 962 453</a></p>--}}
+{{--            </div>--}}
+
+{{--        </div>--}}
+
+
+{{--    </div>--}}
+{{--</footer>--}}
+
+<footer id="footer-new">
+    <div class="footer-new-top">
+
+        <div class="footer-new-column">
+
+            <img src="{{ asset('media/logos/vetpartners-logo-1x.png') }}">
+
+            <p>Join Us. Grow Together</p>
+
+            <div class="footer-new-social-networks">
+                <div class="footer-new-social-network">
+                    <a rel="noopener noreferrer" href="https://www.facebook.com/vetpartnersanz/" target="_blank">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+
+                </div>
+                <div class="footer-new-social-network">
+                    <a rel="noopener noreferrer" href="https://www.instagram.com/vetpartners/" target="_blank">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </div>
+                <div class="footer-new-social-network">
+                    <a rel="noopener noreferrer" href="https://www.linkedin.com/company/7792003/" target="_blank">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+                </div>
             </div>
 
         </div>
+        <div class="footer-new-column footer-new-contact">
+            <p>Contact Us</p>
 
+            <ul>
+                <li>
+                    <i class="fas fa-phone-alt"></i>
+                    <span>
+                        Australia:
+                        <br>
+                        <a href="tel:1800 881 864">1800 881 864</a>
+                    </span>
+                </li>
+                <li>
+                    <i class="fas fa-phone-alt"></i>
+                    <span>
+                        New Zealand:
+                        <br>
+                        <a href="tel:0800 426 488">0800 426 488</a>
+                    </span>
+                </li>
+            </ul>
+        </div>
 
+    </div>
+    <div class="footer-new-bottom">
+        <p>
+            <a href="https://vet.partners//privacy-policy/">Privacy Policy</a> |
+            <a href="https://vet.partners//privacy-collection-notice/">Privacy Collection Notice</a> |
+            <a href="https://vet.partners//terms-and-conditions/">Terms and Conditions</a>
+        </p>
+        <p>© {{ date('Y') }} VetPartners | All Rights Reserved</p>
     </div>
 </footer>
 <!-- END Footer -->
